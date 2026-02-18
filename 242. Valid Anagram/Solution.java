c3a56392445dd9d01a56002ad53837875fd04724
Laveena Armakar <liarmarkar@gmail.com>
2026-02-18 17:56:09 +0530

    Add isAnagram method in Solution class
    Implement isAnagram method to check if two strings are anagrams.

---
+++ 242. Valid Anagram/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        char[] char1 = s.toCharArray();
+        char[] char2 = t.toCharArray();
+
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+
+        for(int i=0; i<s.length(); i++){
+            if(char1[i] != char2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
