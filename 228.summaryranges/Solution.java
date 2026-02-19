4025f62eeecb00a967687a2b537a33b76f34f4d6
Laveena Armakar <liarmarkar@gmail.com>
2026-02-19 19:15:33 +0530

    Implement summaryRanges method to summarize ranges

---
+++ 228.summaryranges/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> li = new ArrayList<>();
+
+        for(int i = 0; i<nums.length; i++){
+           int start = nums[i];
+           while(i+1<nums.length && nums[i]+1==nums[i+1]){
+            i++;
+           }
+           if(start!=nums[i]){
+            li.add(start+"->"+nums[i]);
+           }
+           else{
+            li.add(start+"");
+           }
+        }
+        return li;
+        
+    }
+}
