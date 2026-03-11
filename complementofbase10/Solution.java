dc55cac4b81e36ccbf14dbfa5110a0dd126cc9f4
Laveena Armakar <liarmarkar@gmail.com>
2026-03-11 18:22:39 +0530

    Implement bitwise complement function

---
+++ complementofbase10/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int bits = Integer.toBinaryString(n).length();
+        int mask = (1 << bits) - 1;
+        int complement = n ^ mask;
+
+        return complement;
+    }
+}
